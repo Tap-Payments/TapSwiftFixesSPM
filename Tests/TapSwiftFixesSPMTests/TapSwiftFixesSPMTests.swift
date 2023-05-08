d4de8b8a07623ad5ee1523de7a53bb2aa7ba66a2
MahmoudAllam <m.allam@tap.company>
2023-05-08 11:13:46 +0300

    Add spm

---
+++ Tests/TapSwiftFixesSPMTests/TapSwiftFixesSPMTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import TapSwiftFixesSPM
+
+final class TapSwiftFixesSPMTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(TapSwiftFixesSPM().text, "Hello, World!")
+    }
+}
